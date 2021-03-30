d620d6f5e7bdc6097872bb3868fb56f700326fc6
zebanavgurukul <zeba18@navgurukul.org>
2021-03-30 19:13:03 +0530

    commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 require('dotenv/config')
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 // Database
 mongoose.connect(process.env.DB_connect,{useNewUrlParser : true, useUnifiedTopology: true })
     .then(() => {
@@ -15,13 +18,63 @@ mongoose.connect(process.env.DB_connect,{useNewUrlParser : true, useUnifiedTopol
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info : {
+            title : "Technical Project API",
+            version : "1.0.0"
+        }
+    },
+    apis: ["index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// console.log(swaggerDocs);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 // controllers
 const councilControl = require("./controllers/Apis");
 
 // Routes
 app.post("/api/uploadfile", councilControl.csvpost);
+/**
+ * @swagger
+ * /api/uploadfile:
+ *  post:
+ *      description: uploadfileCSV
+ *      paramaters:
+ *      - name: title
+ *        description: CSVFile
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      responses:
+ *          201:
+ *              description: Created
+ * 
+ */
+
+ app.get("/api/aggregated", councilControl.getaggregated)
+/**
+ * @swagger
+ * /api/aggregated:
+ *  get:
+ *      description: Get all policy_type
+ *      responses:
+ *          200:
+ *              description: success
+ */
+
 app.get("/api/search/:firstname", councilControl.getsearch);
-app.get("/api/aggregated", councilControl.getaggregated)
+/**
+ * @swagger
+ * /api/search/firstname:
+ *  get:
+ *      description: Get all search/:firstname
+ *      responses:
+ *          200:
+ *              description: success
+ */
 
 // start server
 const PORT = process.env.PORT;
